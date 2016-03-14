44127086443c31318a028e60241dbe002c329809
whiteblueblack <zh13@Mac-29.local>
2016-03-14 20:33:03 +0800

    132
    123

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  20131105744（3.14）
+//
+//  Created by ZH13 on 16/3/14.
+//  Copyright © 2016年 ZH13. All rights reserved.
+//
+
+import Foundation
+var arr = ["Hello","wodeshijie",100,2.3]
+
+print(arr)
+
+var arr1 = []
+var arr2 = String  ()
